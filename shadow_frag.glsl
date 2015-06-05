975520a15a8a36a8b935f31ca5fe382f8c1a2def
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-06-05 13:51:19 +0100

    Initial shadows service

---
+++ shadow_frag.glsl
@@ -0,0 +1,134 @@
+precision mediump float;
+// our texture
+uniform sampler2D dataTexture;
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+uniform vec3 lightPosition;
+uniform vec3 dataShape;
+uniform int nSlices;
+uniform int nSlicesPerRow;
+uniform int maxRow;
+uniform float alphaCorrection;
+uniform vec2 textureShape;
+uniform float texLevels;
+uniform float steps;
+
+float sliceW = dataShape.x;
+float sliceH = dataShape.y;
+
+vec3 getDatumColor(float datum) {
+    vec3 color = vec3(1, 1, 0);
+    return color;
+}
+float getDatumAlpha(float datum) {
+    return datum * alphaCorrection;
+}
+vec3 mapTo2D(in vec3 p, float sliceW, float sliceH, int nSlicesPerRow, int nSlices){
+    float zLevel = floor(p.z * texLevels);
+    float zIndex = floor(zLevel / float(nSlices));
+    float sliceIndex = (zLevel - (zIndex * float(nSlices)));
+    float oSliceRow = floor(sliceIndex / float(nSlicesPerRow));
+    //Rows are now in the opposite order
+    float sliceRow = float(maxRow) - oSliceRow;
+    float sliceCol = sliceIndex - float(nSlicesPerRow) * oSliceRow;
+    
+    vec2 pxy = (p.xy + vec2(sliceCol, sliceRow)) * vec2(sliceW, sliceH);
+    return vec3(pxy, zIndex);
+}
+float sample3DTexture(sampler2D slices, vec3 p, float sliceW, float sliceH, int nSlicesPerRow, int nSlices, float bias){
+    if(any(greaterThan(p, vec3(1.0))) || any(lessThan(p, vec3(0.0))))
+       return 0.0;
+    vec3 position = mapTo2D(p, sliceW, sliceH, nSlicesPerRow, nSlices);
+    float zTile = position.z;
+    vec4 datumRGB = texture2D(slices, position.xy / textureShape, bias);
+    float datum;
+    if (zTile == 0.0){
+        datum = datumRGB.r;
+    }else if (zTile == 1.0){
+        datum = datumRGB.g;
+    }else if (zTile == 2.0){
+        datum = datumRGB.b;
+    }
+    return datum;
+}
+float sample3DTexture(sampler2D slices, vec3 p, float sliceW, float sliceH, int nSlicesPerRow, int nSlices){
+    return sample3DTexture(slices, p, sliceW, sliceH, nSlicesPerRow, nSlices, 0.0);
+}
+vec4 getRGBAfromDataTex(sampler2D tex, vec3 pos, vec3 dataShape, vec2 texShape){
+    // pos.xyz = clamp(pos.xyz, 0.01,0.99);
+    //float datum = sampleAs3DTexture(tex, pos, dataShape, texShape);
+    float datum = sample3DTexture(tex, pos, sliceW, sliceH, nSlicesPerRow, nSlices);
+    vec3 color = getDatumColor(datum);
+    float alpha = getDatumAlpha(datum);
+    return vec4(color.xyz, alpha);
+}
+float getPathRGBA(vec3 startPos, vec3 endPos, float steps, sampler2D tex){
+    /* Calculates the total RGBA values of a given path through a texture */
+    //The direction from the front position to back position.
+    vec3 dir = endPos - startPos;
+    //vec3 dir = vec3(0.0,0.0,1.0);
+
+    float rayLength = length(dir);
+
+    //Calculate how long to increment in each step.
+    float delta = 1.0 / steps;
+    //The increment in each direction for each step.
+    vec3 deltaDirection = normalize(dir) * delta;
+    float deltaDirectionLength = length(deltaDirection);
+    vec3 currentPosition = startPos;
+
+    //The alpha value accumulated so far.
+    float accumulatedAlpha = 0.0;
+
+    //How long has the ray travelled so far.
+    float accumulatedLength = 0.0;
+
+    //vec4 dataSample;
+    vec4 dataSample;
+    float alphaSample;
+    //Perform the ray marching iterations
+    for(int i = 0; i < int(steps); i++){
+        //Get the voxel intensity value from the 3D texture.    
+        dataSample = getRGBAfromDataTex(dataTexture, currentPosition, dataShape, textureShape);
+        //Store the alpha accumulated so far.
+        accumulatedAlpha += dataSample.a;
+    
+        //Advance the ray.
+        currentPosition += deltaDirection;
+        accumulatedLength += deltaDirectionLength;
+                  
+        //If the length traversed is more than the ray length, or if the alpha accumulated reaches 1.0 then exit.
+        if(accumulatedLength >= rayLength || accumulatedAlpha >= 1.0 ){
+            break;
+        }
+    }
+    return min(accumulatedAlpha, 1.0);
+}
+vec3 mapTo3D(vec3 pos, float sliceW, float sliceH, int nSlicesPerRow, int nSlices) {
+	int sliceCol = int(floor(pos.x / sliceW));
+	int sliceRow = int(floor(pos.y / sliceH));
+    //Fix to put rows in the right order
+    int sliceIndex = (((maxRow - sliceRow) * nSlicesPerRow) + (sliceCol));
+	vec2 pxy = (pos.xy / vec2(sliceW, sliceH)) - vec2(sliceCol, sliceRow);
+	//pos.z in {0,1,2}
+	int z = ((int(pos.z) * nSlices) + sliceIndex);
+	float pz = float(z)/texLevels;
+	return vec3(pxy, pz);
+}
+void main() {
+   // Look up a color from the texture.
+   vec4 inPixel = texture2D(dataTexture, v_texCoord);
+   vec3 rInCoord = vec3(v_texCoord*textureShape, 0.0);
+   vec3 gInCoord = vec3(v_texCoord*textureShape, 1.0);
+   vec3 bInCoord = vec3(v_texCoord*textureShape, 2.0);
+   vec3 rPoint = mapTo3D(rInCoord, sliceW, sliceH, nSlicesPerRow, nSlices);
+   vec3 bPoint = mapTo3D(bInCoord, sliceW, sliceH, nSlicesPerRow, nSlices);
+   vec3 gPoint = mapTo3D(gInCoord, sliceW, sliceH, nSlicesPerRow, nSlices);
+   float rOut = getPathRGBA(rPoint, lightPosition, steps, dataTexture);
+   float bOut = getPathRGBA(bPoint, lightPosition, steps, dataTexture);
+   float gOut = getPathRGBA(gPoint, lightPosition, steps, dataTexture);
+   vec3 absorbed = vec3(rOut, gOut, bOut);
+   vec3 light = vec3(1.0) - absorbed;
+   gl_FragColor = vec4(absorbed, 1.0);
+   //gl_FragColor = vec4(v_texCoord, 0.0 ,1.0);
+}
---
+++ shadow_generation.py
@@ -0,0 +1,133 @@
+import numpy as np
+from vispy import app
+from vispy import gloo
+from vispy.io import imsave
+import OpenGL.GL as gl
+from scipy import misc
+
+def getCloudTexture(imgPath, imgWidth, imgHeight):
+	'''
+	Reads an image file, converts from 0-255 to 0-1, and loads into a 2D texture.
+	@type imgPath: string
+	@type imgWidth: number
+	@type ingHeight: number
+	@return: gloo.Texture2D
+	'''
+	cloudImage = misc.imread(imgPath)
+	divisor = np.full((imgWidth, imgHeight, 3), 255.0)
+	cloudScaledImage = np.divide(cloudImage, divisor).astype(np.float32)
+	cloudTexture = gloo.Texture2D(cloudScaledImage)
+	return cloudTexture
+
+def getShader(shaderPath):
+	'''
+	Loads a glsl shader as a string.
+	@type shaderPath: string
+	@return: string
+	'''
+	shaderFile = open(shaderPath, 'rb')
+	shader = shaderFile.read()
+	shaderFile.close()
+	return shader
+
+def mkProgram(vShader, fShader, texture, dataShape, textureShape, tileLayout):
+	'''
+	Sets up a program with the given vertex and fragment shaders, and sets 
+	the following shader attributes:
+		dataTexture, textureShape, u_resolution, dataShape,
+		nSlices, texLevels, nSlicesPerRow, maxRow, a_position
+	@type vShader, fShader: string
+	@type texture: gloo.Texture2D
+	@type dataShape: 3-tuple
+	@type textureShape: 2-tuple
+	@type tileLayout: 2-tuple
+	@return: gloo.Program 
+	'''
+	program = gloo.Program(vShader, fShader, count=4)
+	program['dataTexture'] = texture
+	program['textureShape'] = textureShape
+	program['u_resolution'] = textureShape
+	program['dataShape'] = dataShape
+
+	nTiles = tileLayout[0] * tileLayout[1]
+	program['nSlices'] = nTiles
+	program['texLevels'] = nTiles * 3
+	program['nSlicesPerRow'] = tileLayout[0]
+	program['maxRow'] = tileLayout[1] - 1
+
+	width = textureShape[0]
+	height = textureShape[1]
+	my_positions_array = np.array([ (0, 0), (0, height), (width, 0), (width, height) ])
+	program['a_position'] = gloo.VertexBuffer(my_positions_array.astype(np.float32))
+
+	return program
+
+def setLightPosition(program, position):
+	'''
+	Sets the program's lightPosition attribute.
+	@type program: gloo.Program
+	@type position: 3-tuple
+	'''
+	program['lightPosition'] = position
+
+def setResolution(program, steps, alphaScale):
+	'''
+	Sets the program's steps and alphaCorrection attributes.
+	@type program: gloo.Program
+	@type steps: int
+	@type alphaScale: int
+	'''
+	program['steps'] = steps
+	program['alphaCorrection'] = alphaScale/float(steps)
+
+def drawShadows(inputFile='/Users/rachel/Downloads/cloud_frac_padded_623_812_70_4096_4096.png',
+				outputFile='/Users/rachel/Downloads/shadow.png',
+				lightPosition=(20, 0, 0),
+				dataShape=(623, 812, 70),
+				textureShape=(4096, 4096),
+				tileLayout=(6,5),
+				steps=81,
+				alphaScale=2):
+	'''
+	Given a tiled data PNG file and a light position, computes the shadows
+	on the data and writes them to a second PNG.
+	@param inputFile: path to the input PNG
+	@type inputFile: string
+	@param outputFile: path to write out the results
+	@type outputFile: string
+	@param lightPosition: position of the point light
+	@type lightPosition: 3-tuple
+	@param dataShape: 3D shape of the data field
+	@type dataShape: 3-tuple
+	@param textureShape: shape of the input image
+	@type textureShape: 2-tuple
+	@param tileLayout: (cols, rows) arrangement of tiles in the input PNG
+	@type tileLayout: 2-tuple
+	@param steps: how many steps to take through the data in calculations
+	@type steps: int
+	@param alphaScale: factor to scale the light absorption
+	@type alphaScale: number
+	'''
+	
+	width = textureShape[0]
+	height = textureShape[1]
+
+	c = app.Canvas(show=False, size=(width, height))
+
+	cloudTex = getCloudTexture(inputFile, width, height)
+	vertex = getShader('shadow_vertex.glsl')
+	fragment = getShader('shadow_frag.glsl')
+
+	program = mkProgram(vertex, fragment, cloudTex, dataShape=dataShape, textureShape=textureShape, tileLayout=tileLayout)
+	setLightPosition(program, lightPosition)
+	setResolution(program, steps, alphaScale)
+
+	@c.connect
+	def on_draw(event):
+	    gloo.clear((1,1,1,1))
+	    program.draw(gl.GL_TRIANGLE_STRIP)
+	    im = gloo.util._screenshot((0, 0, c.size[0], c.size[1]))
+	    imsave(outputFile, im)
+	    c.close()
+
+	app.run()
